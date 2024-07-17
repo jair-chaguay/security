b22adea85f8c88fc0f6ec568b3cbfbb16001cc9e
jair-chaguay <jchaguay@espol.edu.ec>
2024-07-17 14:49:57 -0500

    Update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const sequelize = require('../models/index.js').sequelize;
+var initModels= require("../models/init-models.js");
+var models = initModels(sequelize);
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('crud');
+router.get('/', async function(req, res, next) {
+  
+  let usersCollection = await models.users.findAll({ })
+  res.render('crud',{title: 'CRUD with users', usersArray: usersCollection});
 
 });
 
