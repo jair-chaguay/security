0ff031f3fefbb9884711883543f0cf851fb6f9e6
jair-chaguay <jchaguay@espol.edu.ec>
2024-07-22 13:51:02 -0500

    Update

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+let crypto = require('crypto');
+
 const sequelize = require('../models/index.js').sequelize;
 var initModels= require("../models/init-models.js");
 var models = initModels(sequelize);
@@ -13,5 +15,20 @@ router.get('/', async function(req, res, next) {
   res.render('crud',{title: 'CRUD with users', usersArray: usersCollection});
 
 });
+
+router.post('/',async(req, res)=>{
+  let{name, password, idrole}=req.body;
+  try{
+    let salt = process.env.SALT
+    let hash = crypto.createHmac('sha512',salt).update(password).digest("base64");
+    let passwordHash = salt + "$"+ hash
+
+    let user = await models.users.create({name:name, password:passwordHash})
+
+    res.redirect('/users')
+  }catch(error){
+    res.status(400).send(error)
+  }
+})
 
 module.exports = router;
